da0770bae2b70673f3aaa3fd80f63dc31ea84d0e
Evan Zlotnick <evan.zlotnick3@t-mobile.com>
2020-07-01 11:29:52 -0700

    install serverless in fargate container

--- pipeline.sh
+++ pipeline.sh
@@ -3,6 +3,7 @@ set -e
 export AWS_ACCESS_KEY_ID="dummy" # For some reason, everything breaks down if an access key and secret access key are not present
 export AWS_SECRET_ACCESS_KEY="value" # Using fake values fixes the problem without exposing any security concerns...
 npm install
+npm i -g serverless
 sls dynamodb install
 TMPFILE=./offline$$.log
 sls offline start 2>1 > $TMPFILE &
